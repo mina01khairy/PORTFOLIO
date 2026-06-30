f7f9de62cd5730ddf844a15091e81ff626bc2d6b
Mina Khairy <your_email@gmail.com>
2026-06-30 03:03:10 +0300

    second commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,15 @@
 import React from 'react';
 import "./globals.css";
 
+
+export const metadata = {
+  title: "PORTFOLIO",
+  icons: {
+    icon: "/Mina.png",
+  },
+};
+
+
 export default function RootLayout(props) {
   return (
     <html>
